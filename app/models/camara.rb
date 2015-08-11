e3264c06467c5da4797b8ee2788a74bb3e64b033
Juan Lopez <capjuancode@gmail.com>
2015-08-11 00:50:50 -0400

    add style css hover to last text and click to show

--- app/models/camara.rb
+++ app/models/camara.rb
@@ -1,6 +1,6 @@
 class Camara < ActiveRecord::Base
 
-  has_attached_file :img, :styles => {large:"600x600", medium: "300x300", thumb: "150x150" }
+  has_attached_file :img, :styles => {large:"960x960", medium: "720x720", thumb: "300x300" }
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\Z/
   belongs_to :user
   has_many :reviews, dependent: :destroy
